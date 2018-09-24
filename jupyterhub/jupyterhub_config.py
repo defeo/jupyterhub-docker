c9307f55be93a2d019ef148f77ff8b03620adcac
Luca De Feo <defeo@pippin>
2018-09-24 02:44:35 +0200

    Decent beta with Sage + R and user persistence

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -32,13 +32,20 @@ c.Authenticator.admin_users = { 'lucadefe' }
 import os
 
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-c.DockerSpawner.image = 'jupyter/base-notebook:137a295ff71b'
+c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
-c.Spawner.cpu_limit = 1
-c.Spawner.mem_limit = '10G'
-
 # See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
 c.JupyterHub.hub_ip = os.environ['HUB_IP']
+
+# user data persistence
+# see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
+c.DockerSpawner.notebook_dir = notebook_dir
+c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+
+# Other stuff
+c.Spawner.cpu_limit = 1
+c.Spawner.mem_limit = '10G'
 
 
 ## Services
